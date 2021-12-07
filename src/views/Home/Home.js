57420f116a3a7e9261c173c472ffe9b5ad787c67
AyelenBarrale <ayebarrale@gmail.com>
2021-12-07 09:08:59 -0300

    slider on firebase

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,15 +1,30 @@
-import React from "react";
-import "../Home/Home.css"
+import React, { useState, useEffect } from "react";
+import "../Home/Home.css";
 import ItemListContainer from "../../Components/ItemListContainer/ItemListContainer";
 
-import slider from "./slider.png"
+import { db } from "../../firebase";
 
 const Home = () => {
+  const [portadas, setPortadas] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      let sliders = await db
+        .collection("sliders")
+        .get();
+      setPortadas(sliders.docs.map((item) => ({ id: item.id, ...item.data() })));
+    })();
+  }, []);
+
   return (
     <div>
-      <img src={slider} alt="" className="slider"/>
-      <h1 className="home-title">Nuestros productos</h1>
-      
+      {portadas.map((item) => (
+        <div key={item.id}>
+          <img src={item.imagen} alt='' className='slider' />
+        </div>
+      ))}
+      <h1 className='home-title'>Nuestros productos</h1>
+
       <ItemListContainer />
     </div>
   );
